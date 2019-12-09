2760a71f909135195952d787611f2d8abb3205bb
cjogles <jackson.ogles28@gmail.com>
2019-12-09 15:18:39 -0700

    got server to run and get all account resources from data base.

--- account/AccountRouter.js
+++ account/AccountRouter.js
@@ -2,3 +2,17 @@ const express = require('express');
 const knex = require('../data/dbConfig');
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    knex
+      .select("*")
+      .from("accounts")
+      .then(accounts => {
+        res.status(200).json(accounts);
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ errorMessage: "Error getting the posts" });
+      });
+  });
+
+  module.exports = router;
